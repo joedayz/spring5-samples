32ee718aa438fe4491e69e0999073a4cf3ee2c2c
Jose Diaz <jose.diaz@joedayz.pe>
2018-06-08 21:21:52 -0500

    insert data

--- mybatis/demomybatis/src/main/resources/db/data.sql
+++ mybatis/demomybatis/src/main/resources/db/data.sql
@@ -1,8 +1,8 @@
-insert into student
+insert into student (name, passport)
 values('Hugo', 'E1234567');
 
-insert into student
+insert into student (name, passport)
 values('Paco', 'A1234568');
 
-insert into student
+insert into student (name, passport)
 values('Luis', 'A6778345');
