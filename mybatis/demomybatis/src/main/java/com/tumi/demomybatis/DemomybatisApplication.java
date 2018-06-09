c9b11137272786572fc458e70a01a33ae27b7639
Jose Diaz <jose.diaz@joedayz.pe>
2018-06-08 21:29:43 -0500

    command line runner

--- mybatis/demomybatis/src/main/java/com/tumi/demomybatis/DemomybatisApplication.java
+++ mybatis/demomybatis/src/main/java/com/tumi/demomybatis/DemomybatisApplication.java
@@ -1,12 +1,40 @@
 package com.tumi.demomybatis;
 
+import com.tumi.demomybatis.model.Student;
+import com.tumi.demomybatis.repository.StudentMyBatisRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemomybatisApplication {
+public class DemomybatisApplication
+	implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemomybatisApplication.class, args);
+	}
+
+
+	@Autowired
+	private StudentMyBatisRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		logger.info("Student id 10001 -> {}", repository.findById(10001L));
+
+		logger.info("Inserting -> {}", repository.insert(new Student(10010L, "Pepe", "A1234657")));
+
+		logger.info("Update 10003 -> {}", repository.update(new Student(10001L, "Name-Updated", "New-Passport")));
+
+		repository.deleteById(10002L);
+
+		logger.info("All users -> {}", repository.findAll());
+
 	}
 }
